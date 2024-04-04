fb83c67e448bc274a5b88f0aff8afe954f443d78
mylenavitoriano <mylenabarbosa08@gmail.com>
2024-04-04 12:16:43 -0300

    fix: cart provider

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { GlobalMovieProps as Movie} from "@/@types/movie-type";
-import { ReactNode,createContext, useState, useEffect } from "react";
+import { GlobalMovieProps as Movie } from "@/@types/movie-type";
+import { ReactNode, createContext, useState, useEffect } from "react";
 
 export interface CartMovie extends Movie {
     quantity: number
@@ -20,91 +20,96 @@ interface ICartContext {
 export const CartContext = createContext<ICartContext>({
     movies: [],
     cartTotalPrice: 0,
-    addMovieToCart: () => {},
-    decreaseMovieQuantity: () => {},
-    incriseMovieQuantity: () => {},
-    removeMovieFromCart: () => {},
-    removeAllMoviesFromCart: () => {},
+    addMovieToCart: () => { },
+    decreaseMovieQuantity: () => { },
+    incriseMovieQuantity: () => { },
+    removeMovieFromCart: () => { },
+    removeAllMoviesFromCart: () => { },
 });
 
 const CART_MOVIES_KEY = 'cartMovies';
 
 const saveCartMovies = (movies: CartMovie[]) => {
-    localStorage.setItem(CART_MOVIES_KEY, JSON.stringify(movies));
+    if (typeof window !== 'undefined') {
+        localStorage.setItem(CART_MOVIES_KEY, JSON.stringify(movies));
+    }
 }
 
 const loadCartMovies = (): CartMovie[] => {
-  const movies = localStorage.getItem(CART_MOVIES_KEY);
-  return movies ? JSON.parse(movies) : [];
+    if (typeof window !== 'undefined') {
+        const movies = localStorage.getItem(CART_MOVIES_KEY);
+        return movies ? JSON.parse(movies) : [];
+    }
+    return [];
 }
 
-const CartProvider = ({children}: {children: ReactNode}) => {
+const CartProvider = ({ children }: { children: ReactNode }) => {
     const [movies, setMovies] = useState<CartMovie[]>(loadCartMovies);
 
     useEffect(() => {
-      saveCartMovies(movies);
+        saveCartMovies(movies);
     }, [movies]);
 
     const addMovieToCart = (movie: CartMovie) => {
         const movieIsAlreadyOnCart = movies.some(cartMovie => cartMovie.id === movie.id)
 
-        if(movieIsAlreadyOnCart){
-            setMovies((prev) => 
-            prev.map((cartMovie) => {
-            if(cartMovie.id === movie.id){
-                return {
-                ... cartMovie,
-                quantity: cartMovie.quantity + movie.quantity,
-                };
-            }
+        if (movieIsAlreadyOnCart) {
+            setMovies((prev) =>
+                prev.map((cartMovie) => {
+                    if (cartMovie.id === movie.id) {
+                        return {
+                            ...cartMovie,
+                            quantity: cartMovie.quantity + movie.quantity,
+                        };
+                    }
 
-            return cartMovie
-            }) 
-        )
+                    return cartMovie
+                })
+            )
 
-        return;
-        } 
+            return;
+        }
 
-        setMovies((prev) => [...prev, movie]);   
-    } 
+        setMovies((prev) => [...prev, movie]);
+    }
 
     const decreaseMovieQuantity = (movieId: number) => {
-        setMovies(prev => 
-          prev.map(cartMovie => {
-          if(cartMovie.id === movieId){
-            return {
-              ... cartMovie,
-              quantity: cartMovie.quantity - 1,
-            }
-          }
-    
-          return cartMovie;
-        }).filter(cartMovie => cartMovie.quantity > 0));
-      }
-    
-      const incriseMovieQuantity = (movieId: number) => {
-        setMovies(prev => 
-          prev.map(cartMovie => {
-          if(cartMovie.id === movieId){
-            return {
-              ... cartMovie,
-              quantity: cartMovie.quantity + 1,
-            }
-          }
-    
-          return cartMovie;
-        }));
-      }
-    
-      const removeMovieFromCart = (movieId: number) => {
-        setMovies((prev) => 
-          prev.filter((cartMovie) => cartMovie.id !== movieId)
-        );
-      };
+        setMovies(prev =>
+            prev.map(cartMovie => {
+                if (cartMovie.id === movieId) {
+                    return {
+                        ...cartMovie,
+                        quantity: cartMovie.quantity - 1,
+                    }
+                }
 
-      const removeAllMoviesFromCart = () => {
+                return cartMovie;
+            }).filter(cartMovie => cartMovie.quantity > 0));
+    }
+
+    const incriseMovieQuantity = (movieId: number) => {
+        setMovies(prev =>
+            prev.map(cartMovie => {
+                if (cartMovie.id === movieId) {
+                    return {
+                        ...cartMovie,
+                        quantity: cartMovie.quantity + 1,
+                    }
+                }
+
+                return cartMovie;
+            }));
+    }
+
+    const removeMovieFromCart = (movieId: number) => {
+        setMovies((prev) =>
+            prev.filter((cartMovie) => cartMovie.id !== movieId)
+        );
+    };
+
+    const removeAllMoviesFromCart = () => {
         setMovies([]);
-      };
+    };
 
     return (
         <CartContext.Provider
