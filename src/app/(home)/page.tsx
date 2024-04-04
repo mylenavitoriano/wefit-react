d87e74de8e03944e5ffeecf8d008c437c651376c
mylenavitoriano <mylenabarbosa08@gmail.com>
2024-04-04 13:51:16 -0300

    fix: url API

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ import CardMovie from '@/components/CardMovie';
 import { GlobalMovieProps } from '@/@types/movie-type';
 import InputSearch from './Components/InputSearch';
 
-const URL_API = 'http://localhost:3001/products'
+
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -19,10 +19,16 @@ export default function Home() {
 
   const fetchAllData = async () => {
     try{
+      let URI_API = '';
       setLoading(true);
       setError(false);
 
-      const response = await fetch(URL_API, {method: 'GET'});
+      if (typeof window !== 'undefined') {
+        const hostname = window.location.hostname;
+        URI_API = 'http://'+hostname+':3001/products';
+      }
+
+      const response = await fetch(URI_API, {method: 'GET'});
       const data = await response.json();
 
       if(!data){
@@ -38,6 +44,8 @@ export default function Home() {
   }
 
   useEffect(() => {
+    
+
     fetchAllData();
   }, []);
 
